7939196ba7d8036f9f6d6272dbcd1270d9d1f9c9
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-15 13:03:35 +0100

    Tree children
    A tree has children and they are introduced with this commit.

--- src/Tree.py
+++ src/Tree.py
@@ -1,12 +1,25 @@
 class Tree:
     def __init__(self, data = None):
         self._data = data
+        self._children = []
 
     def data(self):
         return self._data
 
+    def children(self):
+        return self._children
+
+    def addChild(self, child):
+        self._children.append(child)
+        
 if __name__ == '__main__':
     assert Tree() != None
 
     assert Tree().data() == None
     assert Tree(1).data() == 1
+
+    assert len(Tree().children()) == 0
+    
+    root, child = Tree(), Tree()
+    root.addChild(child)
+    assert child in root.children()
