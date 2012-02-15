f74af78468818fe2b7d8350778d48526eb7f2b6c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-15 14:15:08 +0100

    OppositeEvaluator introduced
    An OppositeEvaluator is introduced to explicitly signal zero-sum
    games.

--- src/Evaluator.py
+++ src/Evaluator.py
@@ -2,6 +2,18 @@ class IdentityEvaluator:
     def value(self, data):
         return data
 
+class OppositeEvaluator:
+    def __init__(self, evaluator):
+        self._evaluator = evaluator
+
+    def value(self, data):
+        return -1 * self._evaluator.value(data)
 
 if __name__ == '__main__':
-    assert IdentityEvaluator().value(0) == 0
+    identity = IdentityEvaluator()
+    assert identity.value(0) == 0
+    assert identity.value(1) == 1
+
+    opposite = OppositeEvaluator(identity)
+    assert opposite.value(0) == 0
+    assert opposite.value(1) == -1
