4f9d45307603a60fcc65a0d34b437908ef2aa653
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-16 08:14:16 +0100

    Alpha Beta implemented
    Alpha Beta pruning is implemented.
    
    Note: we should perform some more test to have confidence in
    refactoring.

--- src/AlphaBetaTree.py
+++ src/AlphaBetaTree.py
@@ -10,7 +10,11 @@ class AlphaBetaTree(GameTree):
     
     def alphaBeta(self, evaluator, alpha = -Decimal('infinity'), beta = Decimal('infinity')):
         if (self.hasChildren()):
-            return GameTree.evaluate(self, evaluator)
+            for child in self.children():
+                alpha = max(alpha, - child.alphaBeta(OppositeTo(evaluator), -beta, -alpha))
+                if (beta <= alpha):
+                    break                            
+            return alpha
         return evaluator.value(self.data())
 
 if __name__ == '__main__':
@@ -21,3 +25,18 @@ if __name__ == '__main__':
     identity = Evaluator.IdentityEvaluator()
     assert AlphaBetaTree(0).evaluate(identity) == 0
     assert AlphaBetaTree(1).evaluate(identity) == 1
+
+    left = GameTree(0)
+    left.addChild(GameTree(1))
+    left.addChild(GameTree(2))
+    assert left.evaluate(identity) == 2
+
+    right = GameTree(0)
+    right.addChild(GameTree(-1))
+    right.addChild(GameTree(-3))
+    assert right.evaluate(identity) == -1
+
+    root = GameTree(0)
+    root.addChild(left)
+    root.addChild(right)
+    assert root.evaluate(identity) == 1
