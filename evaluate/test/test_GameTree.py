92ad4b0ac5ffcc08c0ef88867b2103b753a328b4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-17 14:00:10 +0100

    GameTree Test
    a unit test for GameTree is added.

---
+++ evaluate/test/test_GameTree.py
@@ -0,0 +1,43 @@
+import unittest
+
+from evaluate.GameTree import GameTree
+from evaluate.Tree import Tree
+from evaluate.Evaluator import IdentityEvaluator
+
+class testGameTree(unittest.TestCase):
+    def testGameTreeInstanceOfTree(self):
+        root = GameTree()
+
+        actual = isinstance(root, Tree)
+
+        self.assertTrue(actual)
+
+    def testEvaluationOfLeaf(self):
+        expected = 0
+        root, evaluator = GameTree(expected), IdentityEvaluator()
+
+        actual = root.evaluate(evaluator)
+
+        self.assertEquals(expected, actual)
+
+    def testEvaluationOfTreeOfDepthOne(self):
+        expected, lessThenExpected = 2, 1
+        root = GameTree(lessThenExpected).addChild(GameTree(lessThenExpected)).addChild(GameTree(expected))
+        evaluator = IdentityEvaluator()
+
+        actual = root.evaluate(evaluator)
+
+        self.assertEquals(expected, actual)
+
+    def testEvaluationOfTreeOfDepthTwo(self):
+        expected = 1
+        left = GameTree(0).addChild(GameTree(1)).addChild(GameTree(2))
+        right = GameTree(0).addChild(GameTree(-1)).addChild(GameTree(-3))
+        root = GameTree(0).addChild(left).addChild(right)
+
+        actual = root.evaluate(IdentityEvaluator())
+
+        self.assertEquals(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
