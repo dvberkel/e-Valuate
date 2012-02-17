289123edaff17222b03ac5ecebef0ab840890d5d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-17 14:48:42 +0100

    Expression test
    An expression test is created.

---
+++ evaluate/test/test_Expression.py
@@ -0,0 +1,51 @@
+import unittest
+
+from evaluate.Expression import *
+
+class testExpression(unittest.TestCase):
+    def testVariableEquality(self):
+        self.assertEquals(Variable('A'), Variable('A'))
+        self.assertNotEquals(Variable('A'), Variable('B'))
+        self.assertNotEquals(Variable('A'), Variable())
+        self.assertNotEquals(Variable(), Variable())
+
+    def testPlusEquility(self):
+        self.assertEquals(Plus(Variable('A'), Variable('B')),Plus(Variable('A'), Variable('B')))
+        self.assertNotEquals(Plus(Variable('A'), Variable('B')),Plus(Variable('A'), Variable()))
+
+    def testMinusEquility(self):
+        self.assertEquals(Minus(Variable('A'), Variable('B')),Minus(Variable('A'), Variable('B')))
+        self.assertNotEquals(Minus(Variable('A'), Variable('B')),Minus(Variable('A'), Variable()))
+
+    def testMultiplyEquility(self):
+        self.assertEquals(Multiply(Variable('A'), Variable('B')),Multiply(Variable('A'), Variable('B')))
+        self.assertNotEquals(Multiply(Variable('A'), Variable('B')),Multiply(Variable('A'), Variable()))
+
+    def testNumberEquility(self):
+        self.assertEquals(Number(0),Number(0))
+        self.assertNotEquals(Number(0),Number(1))
+
+    def testExpressionEquality(self):
+        self.assertNotEquals(Plus(Variable(),Variable()),Variable())
+        self.assertNotEquals(Variable(),Plus(Variable(),Variable()))
+        self.assertNotEquals(Plus(Variable(),Variable()),Number(0))
+        self.assertNotEquals(Number(0),Plus(Variable(),Variable()))
+        self.assertNotEquals(Number(0),Variable())
+        self.assertNotEquals(Variable(),Number(0))
+
+    def testSubstitution(self):
+        self.assertEquals(Minus(Number(0), Variable('B')), Minus(Variable('A'), Variable('B')).substitute({'A': 0}))
+        self.assertEquals(Minus(Number(0), Number(1)), Minus(Number(0), Variable('B')).substitute({'B': 1}))
+
+    def testValueOfABoundExpression(self):
+        self.assertEquals(0, Number(0).value())
+        self.assertEquals(3, Plus(Number(1),Number(2)).value())
+        self.assertEquals(-1, Minus(Number(1),Number(2)).value())
+        self.assertEquals(6,  Multiply(Number(2),Number(3)).value())
+
+    def testVariables(self):
+        self.assertEquals(set([Variable('A'), Variable('B')]), Plus(Variable('A'), Variable('B')).variables())
+        
+
+if __name__ == '__main__':
+    unittest.main()
--- evaluate/test/test_all.py
+++ evaluate/test/test_all.py
@@ -3,6 +3,7 @@ import unittest
 from evaluate.test.test_Tree import testTree
 from evaluate.test.test_GameTree import testGameTree
 from evaluate.test.test_AlphaBetaTree import testAlphaBetaTree
+from evaluate.test.test_Expression import testExpression
 
 
 class EvaluateSuite(unittest.TestSuite):
@@ -11,6 +12,7 @@ class EvaluateSuite(unittest.TestSuite):
         self.addTest(unittest.makeSuite(testTree))
         self.addTest(unittest.makeSuite(testGameTree))
         self.addTest(unittest.makeSuite(testAlphaBetaTree))
+        self.addTest(unittest.makeSuite(testExpression))
 
 if __name__ == '__main__':
     suite = EvaluateSuite()
